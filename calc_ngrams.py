735dd69b7e238ef9d91bdd9a4b5b31dfddca29be
Daniel Brandt <dbbrandt@gmail.com>
2019-07-02 22:12:29 -0400

    Work in progress on adding ngrams to features for XGBoost

---
+++ calc_ngrams.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from source.similarity import calculate_containment
+
+answers = pd.read_csv('data/sag2/answers.csv', dtype={'id':str})
+questions = pd.read_csv('data/sag2/questions.csv', dtype={'id':str})
+
+n = [1,2]
+ngrams  = calculate_containment(questions, answers, n)
+
+df = pd.DataFrame(ngrams, columns=['1','2','correct'])
+df.to_csv('data/sag2/answer_ngrams.csv', index=False)
+
